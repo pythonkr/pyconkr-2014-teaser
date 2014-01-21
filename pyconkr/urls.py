6b99c9ce88134c21cbf182b7893d6334aa367c0a
Bae KwonHan <kwonhan.bae@linecorp.com>
2014-01-21 16:19:49 +0900

    add about in urls.py

--- pyconkr/urls.py
+++ pyconkr/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,7 @@ from pyconkr import views
 
 urlpatterns = patterns('',
     url(r'^$', views.pages.Index.as_view()),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
