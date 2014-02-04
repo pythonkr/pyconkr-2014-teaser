a5907874d7ba2b50e91d4892be68f638c3561166
정경업 <py@smartstudy.co.kr>
2014-02-04 13:03:45 +0900

    로컬 세팅 삭제, 세팅파일 분할 설정, 관련 안내 추가

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -104,10 +104,3 @@ STATICFILES_FINDERS = (
 
 # Service (나중에 db로 옮겨질수도. 일단 개발 진행용.)
 REGISTRATION_OPEN = False
-
-
-# Call local_settings
-try:
-    from pyconkr.local_settings import *
-except ImportError, e:
-    pass
