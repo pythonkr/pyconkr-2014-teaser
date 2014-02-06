86ae58a592c5277d014feb640eeac6678500ecca
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-02-06 21:51:07 +0900

    SiteConfig 관리자에서 조금 더 직관적으로 쓸 수 있도록 작성

--- page/admin.py
+++ page/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from page.models import SiteConfig
 
 
-admin.site.register(SiteConfig)
+class SiteConfigAdmin(admin.ModelAdmin):
+    exclude = []
+
+    def get_form(self, request, obj=None, **kwargs):
+        leaves = {1: 'config_boolean', 2: 'config_integer', 3: 'config_varchar', 4: 'config_text', 5: 'config_datetime'}
+        self.exclude = leaves.values()
+        self.exclude.remove(leaves.get(obj.config_type, None))
+
+        return super(SiteConfigAdmin, self).get_form(request, obj, **kwargs)
+
+
+admin.site.register(SiteConfig, SiteConfigAdmin)
