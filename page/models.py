eed6bf515bf4184a96e516d016ac8d86101eb9cc
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-02-06 21:50:48 +0900

    SiteConfig 모델을 여러가지 타입을 사용할 수 있게 변경

--- page/models.py
+++ page/models.py
@@ -3,4 +3,17 @@ from django.db import models
 
 class SiteConfig(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    swc = models.BooleanField(default=False)
+
+    config_type = models.IntegerField(choices=(
+        (1, u'Boolean'),
+        (2, u'Integer'),
+        (3, u'Character(200)'),
+        (4, u'Text'),
+        (5, u'Datetime'),
+    ), default=1)
+
+    config_boolean = models.NullBooleanField(blank=True, null=True)
+    config_integer = models.IntegerField(blank=True, null=True)
+    config_varchar = models.CharField(max_length=200, blank=True)
+    config_text = models.TextField(blank=True)
+    config_datetime = models.DateTimeField(blank=True, null=True)
