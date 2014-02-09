d877936036fbda5f1cef82164dc45b1906f7965d
lexifdev <sl@lxf.kr>
2014-02-09 17:34:08 +0900

    코드는 실행하고 커밋하자

--- page/models.py
+++ page/models.py
@@ -2,39 +2,21 @@ from django.db import models
 from datetime import datetime
 
 
-class SiteConfig(models.Model):
-    TYPE_BOOLEAN = 1
-    TYPE_INTEGER = 2
-    TYPE_STRING = 3
-    TYPE_DATETIME = 5
-
-    title = models.CharField(max_length=200, unique=True)
-    config_type = models.IntegerField(choices=(
-        (TYPE_BOOLEAN, u'Boolean'),
-        (TYPE_INTEGER, u'Integer'),
-        (TYPE_STRING, u'String'),
-        #(4, u'Text'),
-        (TYPE_DATETIME, u'Datetime'),
-    ), default=1)
-    config_boolean = models.NullBooleanField(blank=True, null=True)
-    config_integer = models.IntegerField(blank=True, null=True)
-    config_varchar = models.CharField(max_length=200, blank=True)
-    # config_text = models.TextField(blank=True)
-    config_datetime = models.DateTimeField(blank=True, null=True)
-
-
 class SiteConfigManager(models.Manager):
     def get_value(self, name, default=None):
-        config = self.get(title=name)
+        try:
+            config = self.get(title=name)
 
-        if config.config_type == SiteConfig.TYPE_BOOLEAN:
-            return config.config_boolean or default
-        elif config.config_type == SiteConfig.TYPE_INTEGER:
-            return config.config_integer or default
-        elif config.config_type == SiteConfig.TYPE_STRING:
-            return config.config_varchar or default
-        elif config.config_datetime == SiteConfig.TYPE_DATETIME:
-            return config.config_datetime or default
+            if config.config_type == SiteConfig.TYPE_BOOLEAN:
+                return config.config_boolean
+            elif config.config_type == SiteConfig.TYPE_INTEGER:
+                return config.config_integer
+            elif config.config_type == SiteConfig.TYPE_STRING:
+                return config.config_varchar
+            elif config.config_datetime == SiteConfig.TYPE_DATETIME:
+                return config.config_datetime
+        except SiteConfig.DoesNotExist as e:
+            return default
 
     def set_value(self, name, value):
         config, created = self.get_or_create()
@@ -58,4 +40,24 @@ class SiteConfigManager(models.Manager):
         config.save()
 
 
-SiteConfig.objects = SiteConfigManager()
+class SiteConfig(models.Model):
+    TYPE_BOOLEAN = 1
+    TYPE_INTEGER = 2
+    TYPE_STRING = 3
+    TYPE_DATETIME = 5
+
+    objects = SiteConfigManager()
+
+    title = models.CharField(max_length=200, unique=True)
+    config_type = models.IntegerField(choices=(
+        (TYPE_BOOLEAN, u'Boolean'),
+        (TYPE_INTEGER, u'Integer'),
+        (TYPE_STRING, u'String'),
+        #(4, u'Text'),
+        (TYPE_DATETIME, u'Datetime'),
+    ), default=1)
+    config_boolean = models.NullBooleanField(blank=True, null=True)
+    config_integer = models.IntegerField(blank=True, null=True)
+    config_varchar = models.CharField(max_length=200, blank=True)
+    # config_text = models.TextField(blank=True)
+    config_datetime = models.DateTimeField(blank=True, null=True)
