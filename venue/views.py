b059c538599245f6d84d3ce9985f21a844f682b3
lexifdev <sl@lxf.kr>
2014-02-17 15:09:32 +0900

    약간의 리팩토링.

--- venue/views.py
+++ venue/views.py
@@ -4,6 +4,7 @@ from django.views.generic.base import View
 
 class VenueView(View):
     template_name = 'venue/venue.html'
+
     def get(self, request):
         ctx = {}
         return render(request, self.template_name, ctx)
