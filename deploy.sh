22360c53f569bf9814e30c47453f4042f556abf9
Bae KwonHan <darjeeling@gmail.com>
2014-03-09 17:30:12 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,6 @@ rm -rf ./collected_static/
 pip install -r requirements.pip
 pip install mysql-python
 ./manage.py flush --noinput --settings=settings.deploy
+./manage.py syncdb --noinput --settings=settings.deploy
 ./manage.py collectstatic --noinput --settings=settings.deploy
 # service reload?
