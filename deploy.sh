1435f67920ce3c9810d596ec7f84f29df6ead771
Bae KwonHan <darjeeling@gmail.com>
2014-03-09 18:36:32 +0900

    update deploy.sh to reload gunicorn

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,5 @@ pip install mysql-python
 ./manage.py flush --noinput --settings=settings.deploy
 ./manage.py syncdb --noinput --settings=settings.deploy
 ./manage.py collectstatic --noinput --settings=settings.deploy
+kill -HUP `cat /home/deploy/pyconkr.pid`
 # service reload?
