f31126aaa5d3f3029a5961fa4776c98f79ad873d
sukkyung.park <sukkyung.park@yogiyo.co.kr>
2014-05-31 20:27:37 +0900

    js 정리

--- _prototype/js/main.js
+++ _prototype/js/main.js
@@ -2,19 +2,34 @@ var pyconKr = (function($) {
 		var $body = $('body'),
 		$window = $(window),
 		doc = document,
-		$link = $('#nav').find('a');
+		$link = $('#nav').find('a'),
+        scrollAnchor, scrollPoint,
+        map, eventLocationMarker;
 
 	function init() {
 		$link.on('click', function() {
-			var scrollAnchor = $(this).attr('data-scroll'),
-       		scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
+			scrollAnchor = $(this).attr('data-scroll');
+            scrollPoint = $('section[data-anchor="' + scrollAnchor + '"]').offset().top - 28;
 
-    		$('body,html').animate({
-        		scrollTop: scrollPoint
-    		}, 500);
+            $('body,html').animate({
+                scrollTop: scrollPoint
+            }, 500);
 
-    		return false;
-		});
+            return false;
+        });
+
+        // google maps
+        google.maps.event.addDomListener(window, 'load', function() {
+            eventLocationMarker = new google.maps.Marker({
+                // 잠실 롯데호텔월드
+                position: new google.maps.LatLng(37.511453, 127.100181)
+            });
+            map = new google.maps.Map(document.getElementById('map-canvas'), {
+                zoom: 15,
+                center: eventLocationMarker.getPosition()
+            });
+            eventLocationMarker.setMap(map);
+        });
 	}
 
 	return {
@@ -26,19 +41,3 @@ $(document).ready(function() {
 	pyconKr.init();
 });
 
-// google maps
-
-var map;
-var eventLocationMarker;
-
-google.maps.event.addDomListener(window, 'load', function() {
-    eventLocationMarker = new google.maps.Marker({
-        // 잠실 롯데호텔월드
-        position: new google.maps.LatLng(37.511453, 127.100181)
-    });
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: eventLocationMarker.getPosition()
-    });
-    eventLocationMarker.setMap(map);
-});
