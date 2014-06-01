bcb059453aeada8418417b2744f353a4dae89a41
woojing <ultra066@gmail.com>
2014-06-01 10:32:35 +0900

    make template work with models data

--- page/views.py
+++ page/views.py
@@ -9,7 +9,7 @@ class Home(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(Home, self).get_context_data(**kwargs)
-        context['programs'] = Program.objects.all()
+        context['programs'] = Program.objects.all().order_by('start')
         context['sponsors'] = Sponsor.objects.all()
         context['speakers'] = Speaker.objects.all()
         context['CACHE_TIMEOUT'] = settings.TEMPLATE_CACHE_TIMEOUT
